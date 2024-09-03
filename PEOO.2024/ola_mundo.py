18406a01ce3f84b2f26b522c048270128526d264
emyyas <161068588+emyyas@users.noreply.github.com>
2024-09-03 17:26:00 -0300

    Update ola_mundo.py

--- PEOO.2024/ola_mundo.py
+++ PEOO.2024/ola_mundo.py
@@ -1,2 +1,39 @@
-print ("ola mundo!")
-print ("Testando o Codespaces")
+class Treinos:
+    treinos = []
+
+    def Inserir(self, treino):
+        self.treinos.append(treino)
+
+    def Listar(self):
+        return self.treinos
+
+    def Listar_Id(self, id):
+        for treino in self.treinos:
+            if treino.id == id:
+                return treino
+        return None
+
+    def MaisRapido(self):
+        treinos_ordenados = sorted(self.treinos, key=lambda treino: treino.distancia / treino.tempo, reverse=True)
+        return treinos_ordenados
+
+    def Atualizar(self, id, novo_treino):
+        for i, treino in enumerate(self.treinos):
+            if treino.id == id:
+                self.treinos[i] = novo_treino
+                return
+
+    def Excluir(self, id):
+        for i, treino in enumerate(self.treinos):
+            if treino.id == id:
+                del self.treinos[i]
+                return
+
+    def Abrir(self, arquivo):
+        with open(arquivo, 'r') as f:
+            dados = json.load(f)
+            self.treinos = [Treino(**treino) for treino in dados]
+
+    def Salvar(self, arquivo):
+        with open(arquivo, 'w') as f:
+            json.dump([treino.__dict__ for treino in self.treinos], f, indent=4)
